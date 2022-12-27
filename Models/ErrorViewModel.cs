d8af5ce9b5d03a9878c83b8eb510875867f9100f
mohsinadeem2016 <mohsinnadeem2016@gmai.com>
2022-12-27 15:45:15 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ecommerce_asp.net_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
